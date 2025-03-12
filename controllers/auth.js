832216ed2bdc3af36c5653a787c875827ac2bf8b
erlopez11 <lopezelainy@gmail.com>
2025-03-11 20:45:25 -0500

    wip: add sign out route

--- controllers/auth.js
+++ controllers/auth.js
@@ -51,7 +51,12 @@ router.post('/sign-in', async (req, res) => {
     };
 
     res.redirect('/');
-})
+});
+
+router.get('/sign-out', (req, res) => {
+    req.session.destroy();
+    res.redirect('/');
+});
 
 
 
